4b688406f0d95a53592a123958849ea436a9eead
hollowdll <pixerchat@gmail.com>
2023-04-24 14:29:20 +0300

    customer deletion

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -3,8 +3,9 @@ import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import { Customer } from "../types/customer";
-import { Stack } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
 import { ValueGetterParams } from "ag-grid-community";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import AddCustomer from "./AddCustomer";
 import EditCustomer from "./EditCustomer";
@@ -27,6 +28,7 @@ function CustomerList() {
     // Edit button column
     {
       headerName: "",
+      width: 50,
       filter: false,
       sortable: false,
       cellStyle: { border: "none" },
@@ -34,6 +36,22 @@ function CustomerList() {
         return (
           <EditCustomer updateCustomer={updateCustomer} params={params} />
         );
+      }
+    },
+    // Delete button column
+    {
+      headerName: "",
+      width: 50,
+      field: "id",
+      filter: false,
+      sortable: false,
+      cellStyle: { border: "none" },
+      cellRenderer: (params: ValueGetterParams<Customer>) => {
+        return (
+          <IconButton onClick={() => deleteCustomer(params.getValue("id"))} color="error">
+            <DeleteIcon />
+          </IconButton>
+        )
       }
     }
   ];
@@ -116,10 +134,32 @@ function CustomerList() {
       .catch((err) => console.error(err));
   }
 
+  // Delete a customer
+  const deleteCustomer = (id: number) => {
+    console.log("DELETE CUSTOMER")
+    console.log(id);
+
+    if (window.confirm("Are you sure you want to delete this customer?")) {
+      fetch(`${API_HOST_URL}/api/customers/${id}`, {
+        method: "DELETE",
+      })
+        .then((response) => {
+          if (!response.ok) {
+            setMessage("Failed to delete customer");
+            throw new Error("Fetch failed: " + response.statusText);
+          }
+
+          setMessage("Customer deleted successfully");
+          fetchCustomers();
+        })
+        .catch((err) => console.error(err));
+    }
+  }
+
   return (
     <div>
       <h1>Customers</h1>
-      <p>{message}</p>
+      <h3>{message}</h3>
       <Stack spacing={2} direction="row">
         <AddCustomer addCustomer={addCustomer} />
       </Stack>
