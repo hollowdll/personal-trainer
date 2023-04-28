866b71f023bebcd4f521cde5ed8bfbd04ac3d7db
hollowdll <pixerchat@gmail.com>
2023-04-28 15:23:38 +0300

    calendar for trainings

--- src/components/TabMenu.tsx
+++ src/components/TabMenu.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { AppBar, Tabs, Tab } from '@mui/material';
 import CustomerList from './CustomerList';
 import TrainingList from './TrainingList';
+import TrainingCalendar from './TrainingCalendar';
 
 function TabMenu() {
   const [value, setValue] = useState("customers");
@@ -16,10 +17,12 @@ function TabMenu() {
         <Tabs value={value} onChange={handleChange} textColor="inherit">
           <Tab value="customers" label="Customers" />
           <Tab value="trainings" label="Trainings" />
+          <Tab value="calendar" label="Calendar" />
         </Tabs>
       </AppBar>
       {value === "customers" && <CustomerList />}
       {value === "trainings" && <TrainingList />}
+      {value === "calendar" && <TrainingCalendar />}
     </>
   );
 }
---
+++ src/components/TrainingCalendar.tsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
+
+import { Training } from "../types/training";
+import { API_HOST_URL } from "../utils/const";
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+interface TrainingEvent {
+  start: Date,
+  end: Date,
+  title: string,
+}
+
+function TrainingCalendar() {
+  const [trainings, setTrainings] = useState<Array<Training>>([]);
+  const [eventsList, setEventsList] = useState<Array<TrainingEvent>>([]);
+
+  const fetchTrainings = () => {
+    fetch(`${API_HOST_URL}/gettrainings`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch trainings data");
+        }
+        return response.json();
+      })
+      .then(data => setTrainings(data))
+      .catch(err => console.error(err));
+  }
+
+  // Create calendar event of training
+  const createEvent = (training: Training): TrainingEvent => {
+    const customer = training?.customer ? training.customer : null;
+    const customerName = customer ? `${customer.firstname} ${customer.lastname}` : "No customer found";
+    const startDate = new Date(training.date);
+
+    return {
+      start: startDate,
+      end: new Date(startDate.getTime() + (training.duration * 60 * 1000)),
+      title: `${training.activity} / ${customerName}`,
+    }
+  }
+
+  useEffect(() => {
+    fetchTrainings();
+  }, []);
+
+  useEffect(() => {
+    setEventsList(trainings.map(value => createEvent(value)));
+  }, [trainings]);
+
+  return (
+    <div>
+    <Calendar
+      className="calendar"
+      localizer={localizer}
+      events={eventsList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+  )
+}
+
+export default TrainingCalendar;
